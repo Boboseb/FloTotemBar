70638469b213b3989fcbeaf450eb37688842e0b1
Floraline <floraline@ophiuchus.org>
2011-04-29 01:48:12 +0200

    Initialisation v4.1.20

---
+++ data.lua
@@ -0,0 +1,85 @@
+FLO_TOTEM_SPELLS = {
+	["HUNTER"] = {
+		["TRAP"] = {
+			{ id = 13795, id2 = 82945, school = 1 },
+			{ id = 1499, id2 = 60192, school = 2 },
+			{ id = 13809, id2 = 82941, school = 2 },
+			{ id = 13813, id2 = 82939, school = 1 },
+			{ id = 34600, id2 = 82948, school = 3 },
+			{ id = 77769, duration = 15, school = 4 }
+		}
+	},
+	["SHAMAN"] = {
+		["CALL"] = {
+			{ id = 36936 },
+			{ id = 66842 },
+			{ id = 66843 },
+			{ id = 66844 }
+		},
+		["EARTH"] = {
+			{ id = 8075 },
+			{ id = 2484 },
+			{ id = 8071 },
+			{ id = 8143 },
+			{ id = 2062 },
+			{ id = 5730 }
+		},
+		["FIRE"] = {
+			{ id = 3599 },
+			{ id = 8227 },
+			{ id = 8190 },
+			{ id = 2894 }
+		},
+		["WATER"] = {
+			{ id = 5394 },
+			{ id = 5675 },
+			{ id = 8184 },
+			{ id = 87718 },
+			{ id = 16190 }
+		},
+		["AIR"] = {
+			{ id = 8512 },
+			{ id = 8177 },
+			{ id = 3738 }
+		}
+	}
+};
+FLO_TOTEM_LAYOUTS = {
+	["1row"] = { label = FLO_TOTEM_1ROW, offset = 0,
+		["FloBarFIRE"] = { "LEFT", "FloBarEARTH", "RIGHT", 3, 0 },
+		["FloBarWATER"] = { "LEFT", "FloBarFIRE", "RIGHT", 3, 0 },
+		["FloBarAIR"] = { "LEFT", "FloBarWATER", "RIGHT", 3, 0 },
+		["FloBarCALL"] = { "RIGHT", "FloBarEARTH", "LEFT", -3, 0 },
+	},
+	["2rows"] = { label = FLO_TOTEM_2ROWS, offset = 1,
+		["FloBarFIRE"] = { "LEFT", "FloBarEARTH", "RIGHT", 3, 0 },
+		["FloBarWATER"] = { "TOPLEFT", "FloBarEARTH", "BOTTOMLEFT", 0, 0 },
+		["FloBarAIR"] = { "LEFT", "FloBarWATER", "RIGHT", 3, 0 },
+		["FloBarCALL"] = { "RIGHT", "FloBarEARTH", "LEFT", -3, 0 },
+	},
+	["4rows"] = { label = FLO_TOTEM_4ROWS, offset = 3,
+		["FloBarFIRE"] = { "TOPLEFT", "FloBarEARTH", "BOTTOMLEFT", 0, 0 },
+		["FloBarWATER"] = { "TOPLEFT", "FloBarFIRE", "BOTTOMLEFT", 0, 0 },
+		["FloBarAIR"] = { "TOPLEFT", "FloBarWATER", "BOTTOMLEFT", 0, 0 },
+		["FloBarCALL"] = { "RIGHT", "FloBarEARTH", "LEFT", -3, 0 },
+	},
+	["2rows-reverse"] = { label = FLO_TOTEM_2ROWS_REVERSE, offset = 0,
+		["FloBarFIRE"] = { "LEFT", "FloBarEARTH", "RIGHT", 3, 0 },
+		["FloBarWATER"] = { "BOTTOMLEFT", "FloBarEARTH", "TOPLEFT", 0, 0 },
+		["FloBarAIR"] = { "LEFT", "FloBarWATER", "RIGHT", 3, 0 },
+		["FloBarCALL"] = { "RIGHT", "FloBarEARTH", "LEFT", -3, 0 },
+	},
+	["4rows-reverse"] = { label = FLO_TOTEM_4ROWS_REVERSE, offset = 0,
+		["FloBarFIRE"] = { "BOTTOMLEFT", "FloBarEARTH", "TOPLEFT", 0, 0 },
+		["FloBarWATER"] = { "BOTTOMLEFT", "FloBarFIRE", "TOPLEFT", 0, 0 },
+		["FloBarAIR"] = { "BOTTOMLEFT", "FloBarWATER", "TOPLEFT", 0, 0 },
+		["FloBarCALL"] = { "RIGHT", "FloBarEARTH", "LEFT", -3, 0 },
+	},
+}
+FLO_TOTEM_LAYOUTS_ORDER = {
+	"1row",
+	"2rows",
+	"4rows",
+	"2rows-reverse",
+	"4rows-reverse"
+}
