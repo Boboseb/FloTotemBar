1b62314e67c32f8f139dc40894af9b2194033d01
Floraline <floraline@ophiuchus.org>
2019-09-17 21:39:13 +0200

    Bump version

--- FloTotemBar.lua
+++ FloTotemBar.lua
@@ -8,9 +8,9 @@
 
 local VERSION
 if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then
-	VERSION = "8.2.38"
+	VERSION = "8.2.39"
 elseif WOW_PROJECT_ID == WOW_PROJECT_CLASSIC then
-	VERSION = "1.13.38"
+	VERSION = "1.13.39"
 end
 
 -------------------------------------------------------------------------------
